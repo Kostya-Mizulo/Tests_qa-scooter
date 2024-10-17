c44d58525acd5ab3e535aaca53ba6255b9ef6de9
Konstantin Mizulo <kosyak.mizulo@gmail.com>
2024-10-17 22:41:18 +0300

    Прописал базовые настройки для webDriver. Потенциально придется менять, чтобы работало при запуске тестов. Пока без проверки

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,38 @@
 package tests;
 
-public class BaseTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
+abstract public class BaseTest {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp(){
+        driver = initChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(7, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+
+
+    private WebDriver initChromeDriver(){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+    private WebDriver initFirefoxDriver(){
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
 }
