c6e706fd5120d57fa3452091daa93c0dd61b18f0
Konstantin Mizulo <kosyak.mizulo@gmail.com>
2024-10-19 18:44:25 +0300

    готовы тесты FAQ

---
+++ src/test/java/tests/FAQTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import ext_file_readers.TestValuesProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import page_object.QAScooterMainPage;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class FAQTest extends BaseTest{
+    private final String question;
+    private final String answer;
+
+
+    public FAQTest(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getFAQValues(){
+        return TestValuesProvider.FAQ_VALUES;
+    }
+
+    @Test
+    public void clickQuestionInFAQCorrectAnswerOpensSuccessfully(){
+        QAScooterMainPage mainPage = new QAScooterMainPage();
+        mainPage.scrollToAccordion();
+        By.xpath("//div[@class='accordion__button' and contains(text(), 'Сколько это стоит')]/ancestor::div[@class='accordion__item']//div[@class='accordion__panel']");
+
+        String locatorToAccordionItemQuestion = ((new StringBuilder())
+                .append("//div[@class='accordion__button' and contains(text(), '")
+                .append(question)
+                .append("')]"))
+                .toString();
+
+
+        driver.findElement(By.xpath(locatorToAccordionItemQuestion)).click();
+
+        String locatorToAccordionItemAnswer = ((new StringBuilder())
+                .append(locatorToAccordionItemQuestion)
+                .append("/ancestor::div[@class='accordion__item']//div[@class='accordion__panel']"))
+                .toString();
+
+        String actualTextInAccordion = driver.findElement(By.xpath(locatorToAccordionItemAnswer)).getText();
+
+        assertTrue("Текст ответа не совпадает с ожидаемым", actualTextInAccordion.contains(answer));
+
+    }
+
+
+}
